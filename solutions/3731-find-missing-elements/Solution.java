8ed4ab9ac836037a051175165c3fb48840994bd8
sasitha <karthisasitha@gmail.com>
2026-08-04 23:07:34 +0530

    Add LeetCode solutions

---
+++ solutions/3731-find-missing-elements/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        
+        List<Integer> result=new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int num :nums){
+            set.add(num);
+        }
+        int smallest =nums[0];
+        int largest =nums[0];
+
+        for(int num : nums){
+            if(num<smallest){
+                smallest =num;
+            }
+            if(num >largest){
+                largest =num;
+            }
+            
+        }
+        for(int i=smallest;i<= largest;i++){
+            if(!set.contains(i)){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
